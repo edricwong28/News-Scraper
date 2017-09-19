f774ec05b68a5cdcdeefd2c4b19881dc638faf07
edricwong28 <edricwong28@gmail.com>
2017-09-19 12:39:20 -0700

    updates model and controller files

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,18 +1,13 @@
-// scrape script
-// =============
 
-// Require request and cheerio, making our scrapes possible
+// Require request and cheerio
 var request = require("request");
 var cheerio = require("cheerio");
 
-// This function will scrape the NYTimes website (cb is our callback)
+//scrape the NYTimes website (cb is our callback)
 var scrape = function(cb) {
   // Use the request package to take in the body of the page's html
   request("http://www.nytimes.com", function(err, res, body) {
     // body is the actual HTML on the page. Load this into cheerio
-
-    // Saving this to $ creates a virtual HTML page we can minipulate and
-    // traverse with the same methods we'd use in jQuery
     var $ = cheerio.load(body);
 
     // Make an empty array to save our article info
@@ -22,16 +17,10 @@ var scrape = function(cb) {
     // (i.e, the section holding the articles)
     $(".theme-summary").each(function(i, element) {
       // In each .theme-summary, we grab the child with the class story-heading
-
-      // Then we grab the inner text of the this element and store it
-      // to the head variable. This is the article headline
       var head = $(this).children(".story-heading").text().trim();
 
-      // Grab the URL of the article
       var url = $(this).children(".story-heading").children("a").attr("href");
-
-      // Then we grab any children with the class of summary and then grab it's inner text
-      // We store this to the sum variable. This is the article summary
+      
       var sum = $(this).children(".summary").text().trim();
 
       // So long as our headline and sum and url aren't empty or undefined, do the following
